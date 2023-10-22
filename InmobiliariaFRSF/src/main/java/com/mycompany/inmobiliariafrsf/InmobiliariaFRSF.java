3b4f2d4d870ba72e44f40f8f9dc0e92e6184a43b
SantiagoVulturius <svulturius@outlook.com>
2023-10-21 21:08:17 -0300

    Primer commit

---
+++ InmobiliariaFRSF/src/main/java/com/mycompany/inmobiliariafrsf/InmobiliariaFRSF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inmobiliariafrsf;
+
+/**
+ *
+ * @author Vulturius
+ */
+public class InmobiliariaFRSF {
+
+    public static void main(String[] args) {
+        System.out.println("At the beginning");
+    }
+}
